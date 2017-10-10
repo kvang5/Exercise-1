70d9ac4f683e96fe7fd2d7697fbfeb359d246246
kvang5 <kvang0323@gmail.com>
2017-10-10 14:49:08 -0500

    Added Lombok to Exercise.

--- src/main/java/edu/matc/entity/User.java
+++ src/main/java/edu/matc/entity/User.java
@@ -1,5 +1,8 @@
 package edu.matc.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -12,6 +15,7 @@ import java.time.temporal.ChronoUnit;
  *
  * @author pwaite
  */
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -36,116 +40,6 @@ public class User {
     @Column(name = "user_pass")
     private String userPass;
 
-
-    /**
-     * Instantiates a new User.
-     */
-    public User() {
-    }
-
-    /**
-     * Instantiates a new User.
-     *
-     * @param firstName   the first name
-     * @param lastName    the last name
-     * @param userid      the userid
-     * @param dateOfBirth the date of birth
-     */
-    public User(String firstName, String lastName, String userid, LocalDate dateOfBirth) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userid = userid;
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    /**
-     * Gets first name.
-     *
-     * @return the first name
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Sets first name.
-     *
-     * @param firstName the first name
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Gets last name.
-     *
-     * @return the last name
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Sets last name.
-     *
-     * @param lastName the last name
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Gets userid.
-     *
-     * @return the userid
-     */
-    public String getUserid() {
-        return userid;
-    }
-
-    /**
-     * Sets userid.
-     *
-     * @param userid the userid
-     */
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    /**
-     * Gets date of birth.
-     *
-     * @return the date of birth
-     */
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    /**
-     * Sets date of birth.
-     *
-     * @param dateOfBirth the date of birth
-     */
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPass() {
-        return userPass;
-    }
-
-    public void setUserPass(String userPass) {
-        this.userPass = userPass;
-    }
-
     /**
      * Calculate age int.
      *
@@ -156,15 +50,5 @@ public class User {
         long age = ChronoUnit.YEARS.between(dateOfBirth, now);
 
         return (int)age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", userid='" + userid + '\'' +
-                ", dateOfBirth=" + dateOfBirth +
-                '}';
     }
 }
